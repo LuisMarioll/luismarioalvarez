5c90d3402d5f09f93a6a336860c05d7d767c46e3
LuisMarioll <luisma26a@gmail.com>
2023-06-02 10:51:25 -0500

    menu-subitems

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let hamburguer = document.getElementById("hamburguer");
+let mainNav = document.getElementById("mainNav");
+let mainMenu = document.getElementById("mainMenu");
+
+menuBar.addEventListener("click", function() {
+    if (mainMenu.classList.contains("main-menu")) {
+        mainMenu.classList.add("main-menu-block");
+        mainMenu.classList.remove("main-menu");
+    }
+    else {
+        mainMenu.classList.add("main-menu");
+        mainMenu.classList.remove("main-menu-blovk");
+    }
+});
