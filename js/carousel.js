2d28cdd3aef25dd1c6ca33d41b913f893f40a66b
LuisMarioll <luisma26a@gmail.com>
2023-09-15 10:11:01 -0500

    discord

---
+++ js/carousel.js
@@ -0,0 +1,14 @@
+
+let iconChevronRight = document.getElementById("iconChevronRight");
+let boxCarousel = document.getElementById("boxCarousel");
+
+iconChevronRight.addEventListener("click", function() {
+    if (boxCarousel.classList.contains("box-carousel-1")) {
+        boxCarousel.classList.remove("box-carousel-1");
+        boxCarousel.classList.add("box-carousel-2");
+    }
+    else if (boxCarousel.classList.contains("box-carousel-2")) {
+        boxCarousel.classList.remove("box-carousel-2");
+        boxCarousel.classList.add("box-carousel-3");
+    }
+});
